c75074c9cefb855493dc84a64e832076230f8dec
Nabeel Elnahal <nabeele1@gmail.com>
2026-04-10 08:50:18 -0400

    Question.js schema created

--- models/Question.js
+++ models/Question.js
@@ -1,8 +1,57 @@
 import mongoose from "mongoose";
 
-const questionSchema = new mongoose.Schema({
-  // TODO: Define schema fields
-});
+const questionSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    tags: {
+      type: [String],
+      default: []
+    },
+    upvotes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: []
+      }
+    ],
+    downvotes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: []
+      }
+    ],
+    voteCount: {
+      type: Number,
+      default: 0
+    },
+    views: {
+      type: Number,
+      default: 0
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  {
+    timestamps: true
+  }
+);
 
 const Question = mongoose.model("Question", questionSchema);
 export default Question;
