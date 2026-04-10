ae5a19ae0a5d97a9245610382675a982eb8ca456
Nabeel Elnahal <nabeele1@gmail.com>
2026-04-10 08:44:19 -0400

    User.js schema created

--- models/User.js
+++ models/User.js
@@ -1,8 +1,32 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  // TODO: Define schema fields
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  {
+    timestamps: true
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 export default User;
